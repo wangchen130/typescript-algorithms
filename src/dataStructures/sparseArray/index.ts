5dbb75bd2deb4db2044db791d35886d2e556b9c7
wangchen <wangchen130166@gmail.com>
2023-02-17 15:48:47 +0800

    add: 克鲁斯卡尔算法代码实现。

--- src/dataStructures/sparseArray/index.ts
+++
@@ -1,8 +0,0 @@
-import SparseArray from './SparseArray';
-
-export const testSparseArray = () => {
-  const sparseArray = new SparseArray(11, 11);
-  console.log('====================SparseArray测试开始=============');
-  sparseArray.getSparseArray();
-  console.log(sparseArray);
-};
